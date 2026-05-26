10ccd70ebf0a61e6ca35a515fe97c4bc0eb808c6
godossalvador-sudo <godossalvador@gmail.com>
2026-05-26 00:35:44 -0500

    Update Settings.js

--- src/Settings.js
+++ src/Settings.js
@@ -11,7 +11,7 @@ const value = Object.seal({
     serverFrequency: 25,
     serverName: "A Sigmally Server", // OG: "An unnamed server"
     serverGamemode: "FFA",
-    serverPassword: "702",
+    serverPassword: "",
 
     chatEnabled: true,
     /** @type {string[]} */
