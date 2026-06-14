b9269c44d2c44330d0dd3d3f27696e040b202d5c
T4 Godos <godossalvador@gmail.com>
2026-06-14 13:01:43 -0500

    add health check

--- start.js
+++ start.js
@@ -1,4 +1,12 @@
 const { spawn } = require('child_process');
+const http = require('http');
+
+// Health check para Railway
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('OK');
+});
+server.listen(process.env.PORT || 8080);
 
 const game = spawn('node', ['cli/index.js'], { stdio: 'inherit' });
 const console_ = spawn('node', ['console/index.js'], { stdio: 'inherit' });
