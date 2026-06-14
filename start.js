daded7a7b485f337840a3cb5364e168f864ff579
T4 Godos <godossalvador@gmail.com>
2026-06-14 13:05:09 -0500

    fix start

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 const { spawn } = require('child_process');
 const http = require('http');
 
-// Health check para Railway
 const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end('OK');
@@ -9,6 +8,4 @@ const server = http.createServer((req, res) => {
 server.listen(process.env.PORT || 8080);
 
 const game = spawn('node', ['cli/index.js'], { stdio: 'inherit' });
-const console_ = spawn('node', ['console/index.js'], { stdio: 'inherit' });
-
 game.on('close', (code) => process.exit(code));
