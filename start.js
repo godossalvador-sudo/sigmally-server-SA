74a6f0f3f6396e64d0e27ce9f900506cf98bee61
T4 Godos <godossalvador@gmail.com>
2026-06-14 13:23:28 -0500

    fix start solo cli

--- start.js
+++ start.js
@@ -1,11 +1,4 @@
 const { spawn } = require('child_process');
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('OK');
-});
-server.listen(process.env.PORT || 8080);
 
 const game = spawn('node', ['cli/index.js'], { stdio: 'inherit' });
 game.on('close', (code) => process.exit(code));
